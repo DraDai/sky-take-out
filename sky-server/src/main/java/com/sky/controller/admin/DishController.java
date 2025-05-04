df94baab17d99aa6e292c8343b49cb1edac673a7
戴龙星 <124897400+DraDai@users.noreply.github.com>
2025-05-04 08:46:53 +0800

    文件上传功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/dish")
+@Api(tags = "菜品管理")
+public class DishController {
+    private static final Logger log = LoggerFactory.getLogger(DishController.class);
+    private final DishService dishService;
+
+    @Autowired
+    public DishController(DishService dishService) {
+        this.dishService = dishService;
+    }
+
+    @PostMapping
+    @ApiOperation(value = "添加菜品")
+    public Result add(DishDTO dishDTO){
+        log.info("添加菜品，参数：{}", dishDTO);
+        dishService.addWithFlavor(dishDTO);
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "文件更新")
+@Slf4j
+public class FileUploadController {
+    private final AliOssUtil aliOssUtil;
+
+    @Autowired
+    public FileUploadController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传，参数：{}", file);
+        try{
+            //获取文件名
+            String originalFilename = file.getOriginalFilename();
+            //获取后缀名
+            String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //生成新的文件名
+            String objectName = UUID.randomUUID().toString() + suffix;
+            //上传到阿里云OSS
+            String url = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(url);
+        }catch (Exception e) {
+            log.error("文件上传失败", e);
+            return Result.error(MessageConstant.UPLOAD_FAILED);
+        }
+    }
+}
